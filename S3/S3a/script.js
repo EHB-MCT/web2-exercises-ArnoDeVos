eaa9e90320ec121763b74ca190d15f1823a5a70d
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-10-18 23:21:28 +0200

    S3a

--- S3/S3a/script.js
+++ S3/S3a/script.js
@@ -1,35 +1,42 @@
-'use strict';
+"use strict"
 
-window.onload = function() {
-    console.log('loaded')
-    let randomNumber = Math.floor(Math.random() * 21);
-    let inputNumber = document.getElementById('number').value;
-    console.log(randomNumber);
+let pcNumber = '';
 
-    document.getElementById('form').addEventListener('submit', e => {
-        e.preventDefault();
-        compareNumber(inputNumber);
+window.onload = function () {
+    generateNumber();
 
-    })
+    document.getElementById('form').addEventListener('submit', event => {
+        event.preventDefault();
+        let number = document.getElementById('number').value;
+        compareNumber(number).then(
+            result => {
+                document.getElementById('message').innerHTML = result
+            },
+            error => {
+                document.getElementById('message').innerHTML = error
+            }
+        );
+    });
+}
 
-    function compareNumber(n) {
-
-        let inputNumber = document.getElementById('number').value;
-// todo: work further here
-        console.log(randomNumber);
-
-        if(n > randomNumber) {
-            console.log('guess lower');
-        } else if (n < randomNumber) {
-            console.log('guess up');
-        } else if (n == randomNumber) {
-            console.log('You have won');
+function compareNumber(nr) {
+    return new Promise(function (resolve, reject) {
+        if (nr < 21 && nr > -1) {
+            if (pcNumber == nr) {
+                resolve("You've guessed it right. Try again?")
+                generateNumber();
+            } else if (pcNumber < nr) {
+                reject("The number is lower. Guess again")
+            } else if (pcNumber > nr) {
+                reject("The number is higher. Guess again")
+            }
+        } else {
+            reject("Invalid number")
         }
-
-    }
-
-    let promise = new Promise(function(resolve, reject) {
-        console.log('Start of promise');
     })
+}
 
+function generateNumber() {
+    pcNumber = Math.floor(Math.random() * 21);
+    return pcNumber;
 }
