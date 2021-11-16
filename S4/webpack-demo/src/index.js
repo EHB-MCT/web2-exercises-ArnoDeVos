5c3f6acc206db56c6a56b51408f8bb4af0b7108d
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-11-16 13:01:05 +0100

    WIP

--- S4/webpack-demo/src/index.js
+++ S4/webpack-demo/src/index.js
@@ -1,12 +1,45 @@
-import _ from 'lodash';
+import Cleave from 'cleave.js';
+require('cleave.js/dist/addons/cleave-phone.be');
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+new Cleave('.name', {
+    prefix: 'STUDENT-'
+});
+
+new Cleave('.date', {
+    date: true,
+    datePattern: ['d', 'm', 'Y']
+});
+
+new Cleave('.rrn', {
+    blocks: [2, 2, 2, 3, 2],
+    delimiters: ['.', '.', '-', '.']
+});
+
+new Cleave('.age', {
+    numeral: true,
+    numeralPositiveOnly: true
+});
+
+new Cleave('.phone', {
+    phone: true,
+    phoneRegionCode: 'Be'
+});
+
+document.getElementById('form').addEventListener('submit', e => {
+    e.preventDefault()
+
+    let name = document.getElementsByClassName("name")[0].value;
+    let date = document.getElementsByClassName("date")[0].value;
+    let RRN = document.getElementsByClassName("rrn")[0].value;
+    let age = document.getElementsByClassName("age")[0].value;
+    let phone = document.getElementsByClassName("phone")[0].value;
+
+    let htmlString = `
+    <h3>${name} was born on ${date} and is ${age}.
+    <br>
+    The government knows them as ${RRN}.
+    <br>
+    You can contact them at ${phone}`
+
+    document.getElementById("content").innerHTML = htmlString
+})
